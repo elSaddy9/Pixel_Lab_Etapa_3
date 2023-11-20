1a718bceb6e7de7319afc096ae196716e2191afc
saddam_gonzalez <94806284+elSaddy9@users.noreply.github.com>
2023-11-20 06:46:07 -0300

    4-1 a 4-1.6
    -Enemigo Interceptor
    -Ser precavido vale por 2
    -Interceptor diseño
    -Spawn
    -Disparar
    -Colision
    -Para hacer

---
+++ Juego/Naves/Enemigos/EnemigoInterceptor.gd
@@ -0,0 +1,5 @@
+class_name EnemigoInterceptor
+extends EnemigoBase
+
+
+
--- Juego/Naves/Player/Player.gd
+++ Juego/Naves/Player/Player.gd
@@ -1,63 +1,28 @@
 class_name Player
-extends RigidBody2D
-
-##Enums
-enum ESTADO {SPAWNEANDO, VIVO,INVENCIBLE, MUERTO}
+extends NaveBase
 
 ##Atributos export
 export var potencia_motor:int = 20
 export var potencia_rotacion:int = 100
 export var estela_maxima:int = 150
 export var puede_pulsar:bool = true
-export var hitpoints: float = 10.0
-
-
-##Atributos onready
-onready var canion:Canion = $Canion
-onready var laser:RayoLaser = $LaserBeam2D setget ,get_laser
-onready var estela:Estela = $EstelaPocision/Trail2D
-onready var motor_sfx:Motor = $Motor
-onready var colisionador: CollisionShape2D = $CollisionShape2D
-onready var animacion:AnimationPlayer = $AnimationPlayer
-onready var escudo: Escudo = $Escudo setget ,get_escudo
 ##Atributos
 var empuje:Vector2 = Vector2.ZERO
 var dir_rotacion:int = 0
-var estado_actual:int = ESTADO.SPAWNEANDO
+##Atributos onready
+onready var laser:RayoLaser = $LaserBeam2D setget ,get_laser
+onready var estela:Estela = $EstelaPocision/Trail2D
+onready var motor_sfx:Motor = $Motor
+onready var animacion:AnimationPlayer = $AnimationPlayer
+onready var escudo: Escudo = $Escudo setget ,get_escudo
 
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-	
 ##Stters and Getters
 func get_laser() -> RayoLaser:
 	return laser
 	
 func get_escudo() -> Escudo:
 	return escudo
-	
-##Metodos custom
-func controlador_estados(nuevo_estado:int)->void:
-	match nuevo_estado:
-		  ESTADO.SPAWNEANDO:
-					colisionador.set_deferred("disabled",true)
-					canion.set_puede_disparar(false)
-		  ESTADO.VIVO:
-					colisionador.set_deferred("disabled",false)
-					canion.set_puede_disparar(true)
-		  ESTADO.INVENCIBLE:
-					colisionador.set_deferred("disabled",true)
-		  ESTADO.MUERTO:
-					colisionador.set_deferred("disabled",true)
-					canion.set_puede_disparar(false)
-					Eventos.emit_signal("nave_destruida",self,global_position,4)
-					queue_free()
-		  _:
-					printerr("Error de estado")
-	estado_actual = nuevo_estado
-##Metodos
+##Métodos
 func _unhandled_input(event: InputEvent) -> void:
 	if not esta_input_activo():
 		return
@@ -89,7 +54,9 @@ func _integrate_forces(_state: Physics2DDirectBodyState) -> void:
 # warning-ignore:unused_argument
 func _process(delta: float) -> void:
 	player_input()
-		
+
+
+##Metodos Custom	
 func player_input():
 		if not esta_input_activo():
 			return
@@ -119,28 +86,3 @@ func esta_input_activo()->bool:
 	if estado_actual in [ESTADO.SPAWNEANDO,ESTADO.MUERTO]:
 		return false
 	return true
-
-##Señales internas
-#func _on_AnimationPlayer_animation_finished(anim_name:String)->void:
-
-
-func destruir()->void:
-	controlador_estados(ESTADO.MUERTO)
-	
-func recibir_danio(danio: float) -> void:
-	hitpoints -= danio
-	$AudioDanio.play()
-	if hitpoints <= 0.0 :
-		destruir()
-
-
-func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
-	if anim_name == "spawn":
-		controlador_estados(ESTADO.VIVO) # Replace with function body.
-
-
-func _on_Player_body_entered(body: Node) -> void:
-	if body is Meteorito:
-		body.destruir()
-		destruir() # Replace with function body.
-
